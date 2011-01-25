ad4b2662d4a328bc96828ecb835fdf10fc4888d1
Gavin Norman <gavin.norman@sociomantic.com>
2011-01-25 16:33:52 +0000

    + Added new dhtnode application using the asynchronous dht node in swarm.dht2.

---
+++ src/main/dhtnode2.d
@@ -0,0 +1,105 @@
+/*******************************************************************************
+
+    DHT Node Server Daemon
+    
+    copyright:      Copyright (c) 2009 sociomantic labs. All rights reserved
+    
+    version:        Jun 2009: Initial release
+
+    authors:        Thomas Nicolai & Lars Kirchhoff
+
+******************************************************************************/
+
+module src.main.dhtnode2;
+
+/*******************************************************************************
+
+    Imports 
+
+******************************************************************************/
+
+private import  core.config.MainConfig;
+
+private import  mod.server2.DhtNodeServer;
+
+private import  ocean.util.OceanException;
+
+private import  tango.io.Stdout;
+
+private import  tango.util.Arguments;
+
+/*******************************************************************************
+
+    Arguments Handler
+    
+    Checks if command line argument is valid and starts module
+    
+    Params:
+        arguments = array of command line arguments
+    
+    Returns: 
+        false if no or wrong argument is given
+
+******************************************************************************/
+
+bool isArgument ( char[][] arguments )
+{
+    Arguments args = new Arguments;
+    
+    args.prefixShort = ["-"];
+    args.prefixLong  = ["--"];
+    
+    args.define("d").parameters(0).aka("daemon");
+    
+    args.parse(arguments);
+    
+    if ( args.contains("d") )
+    {
+        return OceanException.run(&DhtNodeServer.run);
+    }
+    
+    return false;
+}
+
+/*******************************************************************************
+
+    Print usage
+
+******************************************************************************/
+
+void printUsage ()
+{
+    Stdout.formatln("
+    Usage:
+        dhtnosed [-d]
+                
+    Description:
+        dht node server daemon
+    
+    Parameter:
+        -d, --daemon         start local dht node server
+                
+    Example:
+        dhtnosed -d
+    ");
+}
+
+/*******************************************************************************
+
+    Main (Start)
+    
+    Param:
+        args = command line arguments
+
+******************************************************************************/
+
+void main ( char[][] args )
+{
+    MainConfig.init(args[0]);
+    
+    if (!isArgument(args))
+    {
+        printUsage();
+    }
+}
+
---
+++ src/mod/server2/DhtDaemon.d
@@ -0,0 +1,235 @@
+/*******************************************************************************
+
+    DHT Node Server Daemon
+    
+    copyright:      Copyright (c) 2011 sociomantic labs. All rights reserved
+    
+    version:        June 2009:    Initial release
+                    January 2011: Asynchronous dht node
+    
+    authors:        David Eckardt, Gavin Norman 
+                    Thomas Nicolai, Lars Kirchhoff
+
+******************************************************************************/
+
+module  mod.server2.DhtDaemon;
+
+
+
+/*******************************************************************************
+
+    Imports
+
+******************************************************************************/
+
+private import  core.config.MainConfig;
+
+private import  swarm.dht2.DhtNode;
+private import  swarm.dht2.DhtHash;
+
+private import  swarm.dht2.storage.Memory;
+private import  swarm.dht2.storage.LogFiles;
+
+private import  swarm.dht2.node.model.IDhtNode;
+
+private import  tango.util.log.Log, tango.util.log.AppendConsole;
+
+debug private import tango.util.log.Trace;
+
+
+
+/*******************************************************************************
+
+    DhtDaemon
+
+******************************************************************************/
+
+class DhtDaemon
+{
+    /***************************************************************************
+    
+        alias for Memory node 
+    
+    **************************************************************************/
+    
+    alias       DhtNode!(Memory)                                MemoryNode;
+    
+    /***************************************************************************
+    
+        alias for Memory node 
+    
+    **************************************************************************/
+    
+    alias       DhtNode!(LogFiles, size_t)                      LogFilesNode;
+    
+    /***************************************************************************
+    
+        alias for node item 
+    
+    **************************************************************************/
+    
+    alias       DhtConst.NodeItem       NodeItem;
+    
+    /***************************************************************************
+    
+        alias for storage
+    
+    **************************************************************************/
+    
+    alias       DhtConst.Storage.BaseType   Storage;
+    
+    
+    /***************************************************************************
+    
+        Dht node instance
+    
+    **************************************************************************/
+    
+    private     IDhtNode                node;
+    
+    /***************************************************************************
+    
+         Constructor
+    
+    **************************************************************************/
+    
+    public this ( )
+    {
+        NodeItem node_item = this.getNodeItemConfiguration();
+        
+        this.setLogger();
+        
+        uint    number_threads  = Config.get!(uint)("Server", "connection_threads");
+        ulong   size_limit      = Config.get!(ulong)("Server", "size_limit");
+        char[]  data_dir        = Config.get!(char[])("Server", "data_dir");
+        
+        Storage storage         = this.getStorageConfiguration();
+        
+        if (storage == DhtConst.Storage.None)
+        {
+            throw new Exception("Invalid data storage");
+        }
+        
+        switch (storage)
+        {
+            case DhtConst.Storage.Memory :
+                Trace.formatln("New memory node");
+                this.node = new MemoryNode(node_item, data_dir, size_limit);
+                break;
+        
+            case DhtConst.Storage.LogFiles :
+                this.node = new LogFilesNode(node_item, data_dir, size_limit, this.getLogFilesWriteBuffer());
+                break;
+        
+            default:
+                throw new Exception("Invalid / unsupported data storage");
+                break;
+        }
+    }
+    
+    /***************************************************************************
+    
+        Runs the DHT node
+    
+    **************************************************************************/
+    
+    public int run ()
+    {
+        this.node.eventLoop();
+        
+        return true;
+    }
+    
+    /***************************************************************************
+    
+        Shuts down the DHT node
+    
+    **************************************************************************/
+    
+    public void shutdown ( )
+    {
+        return this.node.shutdown();
+    }
+    
+    /***************************************************************************
+    
+        Reads NodeItem configuration
+    
+    **************************************************************************/
+    
+    private NodeItem getNodeItemConfiguration ()
+    {
+        NodeItem node_item; 
+        
+        char[] minval = Config.getChar("Server", "minval");
+        char[] maxval = Config.getChar("Server", "maxval");
+        
+        node_item.Address  = Config.getChar("Server", "address");
+        node_item.Port     = Config.getInt("Server", "port");
+        node_item.MinValue = DhtHash.toHashRangeStart(minval);
+        node_item.MaxValue = DhtHash.toHashRangeEnd(maxval);
+        
+        return node_item;
+    }
+    
+    /***************************************************************************
+    
+        Set Logger
+    
+    **************************************************************************/
+    
+    private void setLogger ()
+    {
+        auto log = Log.getLogger("dht.persist");
+        log.add(new AppendConsole);
+        log.level = Level.Trace;
+    }
+    
+    /***************************************************************************
+    
+        Get storage configuration
+    
+    **************************************************************************/
+    
+    private Storage getStorageConfiguration ()
+    {
+        switch (Config.get!(char[])("Server", "storage_engine"))
+        {
+            case "hashtable" :
+                return DhtConst.Storage.HashTable;
+        
+            case "btree" : 
+                return DhtConst.Storage.Btree;
+        
+            case "filesystem" : 
+                return DhtConst.Storage.FileSystem;
+        
+            case "memory" : 
+                return DhtConst.Storage.Memory;
+        
+            case "logfiles" :
+                return DhtConst.Storage.LogFiles;
+        
+            default :
+        }
+        
+        return DhtConst.Storage.None;
+    }
+    
+    /***************************************************************************
+    
+        Reads the file output write buffer size for the LogFiles storage engine,
+        using the default if not specified in configuration.
+        
+    **************************************************************************/
+    
+    private size_t getLogFilesWriteBuffer ()
+    {
+    size_t wbs = LogFiles.DefaultWriteBufferSize;
+    
+    Config.get(wbs, "Options_LogFiles", "write_buffer_size");
+    
+    return wbs;
+    }
+}
+
