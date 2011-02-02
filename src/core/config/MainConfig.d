44b1efb3de7d39741fc5e432df3188876169c692
Gavin Norman <gavin.norman@sociomantic.com>
2011-02-02 19:02:29 +0000

    * Hook up TraceLog and OceanException to the log files specified in config.ini.

--- src/core/config/MainConfig.d
+++ src/core/config/MainConfig.d
@@ -63,18 +63,37 @@ public static:
     
     ***************************************************************************/
     
-    void init ( char[] command_path )
+    void init ( char[] exepath )
     {
+        char[] trace_log, error_log;
+        bool trace_enable;
+
         CmdPath cmdpath;
-        cmdpath.set(command_path);
+        cmdpath.set(exepath);
 
         Config.init(cmdpath.prepend("etc", "config.ini"));
-    
+
         // Trace
         show_channel_trace = Config.Bool["Trace", "show_channel_trace"];
         channel_trace_update = Config.Int["Trace", "channel_trace_update"];
         trace_rw_positions = Config.Bool["Trace", "trace_rw_positions"];
         trace_byte_size = Config.Bool["Trace", "trace_byte_size"];
+
+        // Log
+        error_log = Config.Char["Log", "error"];
+        trace_enable = !!Config.Int["Log", "trace_enable"]; 
+
+        if ( trace_enable )
+        {
+            trace_log = Config.Char["Log", "trace"];
+            TraceLog.init(cmdpath.prepend([trace_log]));
+        }
+        else
+        {
+            TraceLog.enabled = false;
+        }
+
+        OceanException.setOutput(new AppendFile(cmdpath.prepend([error_log])));
     }
 }
 
