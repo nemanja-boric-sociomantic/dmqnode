3b04e0658a77f97eff520c41df31da896db7f3e5
Gavin Norman <gavin.norman@sociomantic.com>
2011-02-16 18:24:33 +0000

    + Module & method comments.

--- src/mod/server2/servicethreads/model/IServiceThread.d
+++ src/mod/server2/servicethreads/model/IServiceThread.d
@@ -1,12 +1,31 @@
+/*******************************************************************************
+
+    Dht node service thread abstract base class
+
+    copyright:      Copyright (c) 2011 sociomantic labs. All rights reserved
+
+    version:        February 2011: Initial release
+
+    authors:        Gavin Norman
+
+*******************************************************************************/
+
 module mod.server2.servicethreads.model.IServiceThread;
+
+
+
+/*******************************************************************************
+
+    Imports
+
+*******************************************************************************/
 
 private import mod.server2.util.Terminator;
 
 private import tango.core.Thread;
 
-private import ocean.io.select.model.ISelectListenerInfo;
-
-private import swarm.dht2.node.model.IDhtNode;
+private import swarm.dht2.node.model.IDhtNode,
+               swarm.dht2.node.model.IDhtNodeInfo;
 
 private import swarm.dht2.storage.channels.model.IStorageChannelsService;
 
@@ -16,24 +35,63 @@ debug private import tango.util.log.Trace;
 
 
 
-abstract class IServiceThread : Thread
+public abstract class IServiceThread : Thread
 {
+    /***************************************************************************
+
+        Service interface to the storage channels.
+    
+    ***************************************************************************/
+
     private IStorageChannelsService channels_service;
 
-    private ISelectListenerInfo listener_info;
+
+    /***************************************************************************
+
+        Informational interface to the dht node.
+
+    ***************************************************************************/
+
+    private IDhtNodeInfo node_info;
+
+
+    /***************************************************************************
+
+        Time to sleep between runs of the service.
+
+    ***************************************************************************/
 
     private uint update_time;
+
+
+    /***************************************************************************
+
+        Constructor.
+        
+        Params:
+            dht = dht node to service
+            update_time = time to sleep between runs of the service
+
+    ***************************************************************************/
 
     public this ( IDhtNode dht, uint update_time )
     {
         this.update_time = update_time;
 
-        this.listener_info = dht.selectListenerInfo();
+        this.node_info = dht.nodeInfo();
 
         this.channels_service = dht.channelsService();
 
         super(&this.run);
     }
+
+
+    /***************************************************************************
+
+        Thread main method. Repeatedly sleeps then calls the service methods on
+        the node and on each channel in the node.
+
+    ***************************************************************************/
 
     private void run ( )
     {
@@ -41,7 +99,7 @@ abstract class IServiceThread : Thread
         {
             Thread.sleep(this.update_time);
 
-            this.serviceNode(this.listener_info, this.update_time);
+            this.serviceNode(this.node_info, this.update_time);
 
             foreach ( channel; this.channels_service )
             {
@@ -50,7 +108,30 @@ abstract class IServiceThread : Thread
         }
     }
 
-    abstract protected void serviceNode ( ISelectListenerInfo listener_info, uint seconds_elapsed );
+
+    /***************************************************************************
+
+        Method called on the node info interface once per service run.
+
+        Params:
+            node_info = node information interface
+            seconds_elapsed = time since this service was last performed
+
+    ***************************************************************************/
+
+    abstract protected void serviceNode ( IDhtNodeInfo node_info, uint seconds_elapsed );
+
+
+    /***************************************************************************
+
+        Method called on the channel service interface of all storage channels
+        once per service run.
+
+        Params:
+            channel = channel service interface
+            seconds_elapsed = time since this service was last performed
+
+    ***************************************************************************/
 
     abstract protected void serviceChannel ( IStorageEngineService channel, uint seconds_elapsed );
 }
