d839cde131dcc01cc09895df5928498b24e39649
Gavin Norman <gavin.norman@sociomantic.com>
2011-08-26 15:38:29 +0000

    + Module header.

--- src/mod/server/util/Terminator.d
+++ src/mod/server/util/Terminator.d
@@ -1,4 +1,19 @@
+/*******************************************************************************
+
+    copyright:      Copyright (c) 2010 sociomantic labs. All rights reserved
+    
+    version:        November 2010: Initial release
+    
+    authors:        Gavin Norman
+
+    Static bool indicating that application termination has been requested. Used
+    to synchronize termination of the queue node and the service threads.
+
+*******************************************************************************/
+
 module src.mod.server.util.Terminator;
+
+
 
 struct Terminator
 {
