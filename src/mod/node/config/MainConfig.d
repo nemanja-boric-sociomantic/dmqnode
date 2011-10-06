c1a46638a79a1830a072f8ee3969a03a5be5b790
Gavin Norman <gavin.norman@sociomantic.com>
2011-10-06 10:03:48 +0000

    * Tidied up unused stuff.

--- src/mod/node/config/MainConfig.d
+++ src/mod/node/config/MainConfig.d
@@ -1,6 +1,6 @@
 /*******************************************************************************
 
-    Ad4Max Analytics - General Configuration
+    DhtNode general configuration
 
     copyright:      Copyright (c) 2010 sociomantic labs. All rights reserved
 
@@ -12,100 +12,48 @@
 
 module src.mod.node.config.MainConfig;
 
+
+
 /*******************************************************************************
 
         imports
 
  ******************************************************************************/
 
-private import  swarm.dht.DhtConst;
+private import swarm.dht.DhtConst;
 
-private import  ocean.sys.CmdPath;
+private import ocean.sys.CmdPath;
 
-public  import  ocean.util.Config,
-                ocean.util.OceanException,
-                ocean.util.TraceLog;
+public  import ocean.util.Config,
+               ocean.util.OceanException,
+               ocean.util.TraceLog;
 
-private import  tango.util.log.AppendFile;
+private import tango.util.log.AppendFile;
 
-private import  Date = tango.time.ISO8601,
-                tango.time.Time;
+
 
 /*******************************************************************************
 
     MainConfig structure
 
+    All members are static, used simply as a namespace.
+
  ******************************************************************************/
 
-struct MainConfig
+public struct MainConfig
 {
-public static:
+static:
 
-    /***************************************************************************
-
-        Definitions of configuration section & key strings
-
-     **************************************************************************/
-    
-    public const
-    {
-        struct Path
-        {
-            public static const:
-                char[][] Config         = ["etc", "config.ini"];
-                char[][] DhtNodeCfg     = ["etc", "dhtnodes.xml"];
-        }
-        
-        struct Sections
-        {
-            public static const
-                Server                  = "Server",
-                Options_Hashtable       = "Options_Hashtable",
-                Options_Btree           = "Options_Btree",
-                Options_Memory          = "Options_Memory",                
-                Log                     = "Log";
-        }
-        
-        struct Keys
-        {
-            public static const
-            
-                address                 = "address",
-                port                    = "port",
-                pidfile                 = "pidfile",
-                data_dir                = "data_dir",
-                storage_engine          = "storage_engine",
-                size_limit              = "size_limit",
-                minval                  = "minval",
-                maxval                  = "maxval",
-                
-                bnum                    = "bnum",
-                apow                    = "apow",
-                fpow                    = "apow",
-                compression_mode        = "compression_mode",                
-                lmemb                   = "lmemb",
-                nmemb                   = "nmemb",
-                
-                Error                   = "error",
-                Trace                   = "trace",
-                TraceEnable             = "trace_enable";            
-        }
-    }
-    
     /***************************************************************************
 
         Log
     
     ***************************************************************************/
-    
-    char[] error_log;
-    
-    char[] trace_log;
-    
+
     char[] stats_log;
 
     bool stats_log_enabled;
-    
+
     bool console_stats_enabled;
 
 
@@ -127,19 +75,19 @@ public static:
                       argument 0
     
      **************************************************************************/
-    
-    public void init ( char[] exepath )
-    {   
-        this.cmdpath.set(exepath);
 
-        Config.init(this.cmdpath.prepend(this.Path.Config));
+    public void init ( char[] exepath )
+    {
+        cmdpath.set(exepath);
+
+        Config.init(cmdpath.prepend(["etc", "config.ini"]));
 
         // Log
-        error_log = Config.Char["Log", "error"];
+        auto error_log = Config.Char["Log", "error"];
         OceanException.setOutput(new AppendFile(cmdpath.prepend([error_log])));
         OceanException.console_output = Config.Bool["Log", "console_echo_error"];
 
-        trace_log = Config.Char["Log", "trace"];
+        auto trace_log = Config.Char["Log", "trace"];
         TraceLog.init(cmdpath.prepend([trace_log]));
         TraceLog.console_enabled = Config.Bool["Log", "console_echo_trace"];
 
