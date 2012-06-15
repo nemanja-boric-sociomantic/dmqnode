c475cafe92829803fe094ff500acb7827292019a
Gavin Norman <gavin.norman@sociomantic.com>
2012-06-15 16:17:36 +0000

    * config.ini: * memory.config.ini: * logfiles.config.ini: * Updated example config.ini and split into two   separate files for a logfiles node and a memory node.
    * Terminator.d: + Standard module header.
    
    * DhtNode.dproj:

--- src/core/util/Terminator.d
+++ src/core/util/Terminator.d
@@ -1,4 +1,19 @@
+/*******************************************************************************
+
+    copyright:      Copyright (c) 2012 sociomantic labs. All rights reserved
+
+    version:        15/06/2012: Initial release
+
+    authors:        Gavin Norman
+
+    Namespace struct containing a global boolean which is set to true when the
+    application is terminating (after receiving SIGINT - see IDhtNode).
+
+*******************************************************************************/
+ 
 module src.core.util.Terminator;
+ 
+
 
 struct Terminator
 {
