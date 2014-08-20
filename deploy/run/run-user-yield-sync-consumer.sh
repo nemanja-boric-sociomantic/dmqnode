fa73b0517c918703f0509bc067e5765db4a24236
Gavin Norman <gavin.norman@sociomantic.com>
2014-08-20 18:06:04 +0200

    Add scripts used for AP->US dht sync
    Includes the required upstart scripts, screen config scripts, and sync
    process start/loop scripts.

---
+++ deploy/run/run-user-yield-sync-consumer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+count=1
+
+while :
+do
+	echo "Started Dhtsync $count time(s)"
+	$PWD/dhtsync-64 -S etc/queue.ini  -D etc/dhtmemory.xml  -A --qchannel user_yield -x 10 -X 5
+	sleep 1
+	(( count++ ))
+done
