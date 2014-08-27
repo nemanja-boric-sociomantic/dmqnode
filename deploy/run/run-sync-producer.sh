74606ae6ecad1e19ecd866a7d29a02c99ba0dd11
Gavin Norman <gavin.norman@sociomantic.com>
2014-08-27 19:28:16 +0200

    Also sync campaign_optimization channel from AP->US

--- deploy/run/run-sync-producer.sh
+++ deploy/run/run-sync-producer.sh
@@ -4,7 +4,7 @@ count=1
 while :
 do
         echo "Started Dhtsync $count time(s)"
-	$PWD/dhtsync-64 -S etc/ap-dhtmemory.xml -D etc/queue.ini --qchannel test -x 14 -c purchase_history admedia_categories user_retargeting usermap_16671055276272723590 usermap_2879492446251088096 admedia_metadata admedia_related
+	$PWD/dhtsync-64 -S etc/ap-dhtmemory.xml -D etc/queue.ini --qchannel test -x 14 -c purchase_history admedia_categories user_retargeting usermap_16671055276272723590 usermap_2879492446251088096 admedia_metadata admedia_related campaign_optimization
         sleep 5
 	mv core cores/core_$(date -d "today" +"%Y%m%d%H%M")
         (( count++ ))
