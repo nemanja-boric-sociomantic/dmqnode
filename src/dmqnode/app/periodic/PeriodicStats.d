72badd117498c725c2d9a27c73335ee9998f788c
David Eckardt <david.eckardt@sociomantic.com>
2015-07-08 16:03:09 +0200

    Update the stats logger; update to ocean v1.22
    This is an almost complete rewrite of the periodic stats logger, using
    the new stats log format, tidying up the values to log and adding disk
    overflow statistics.
    
    * submodules/ocean v1.22(bcf579b)...v1.21.2+80(fdef61e) (22 commits)
      < Add missing release notes
      < Fix compile error in ocean.util.ReusableException
      < PeriodicStats.add*(): Allow interface objects
      < Remove 'ref' parameter attribute from public functions where not needed
      < Don't use deprecated assertEx within ocean

--- src/dmqnode/app/periodic/PeriodicStats.d
+++ src/dmqnode/app/periodic/PeriodicStats.d
@@ -25,19 +25,7 @@ private import dmqnode.app.config.StatsConfig;
 
 private import dmqnode.storage.Ring;
 
-private import ocean.core.Traits: FieldName;
-
-private import ocean.math.SlidingAverage;
-
-private import ocean.text.util.DigitGrouping;
-
-private import tango.text.convert.Format;
-
 private import ocean.util.log.Stats;
-
-private import ocean.util.log.StaticTrace;
-
-private import tango.core.Memory;
 
 /*******************************************************************************
 
@@ -51,150 +39,137 @@ public class PeriodicStats : IPeriodic
 {
     /***************************************************************************
 
-        Stats config object, passed into constructor.
+        Definition of the global statistics to log.
 
     ***************************************************************************/
 
-    protected const StatsConfig stats_config;
-
-
-    /***************************************************************************
-
-        Struct containing the values to be written to the stats log file
-
-    ***************************************************************************/
-
-    private struct LogStats
+    struct NodeStats
     {
-        ulong bytes_sent;
+        /***********************************************************************
+
+            The number of open socket connections to clients.
+
+        ***********************************************************************/
+
+        uint handling_connections;
+
+        /***********************************************************************
+
+            The total number of bytes received through the client connections.
+
+        ***********************************************************************/
+
         ulong bytes_received;
-        size_t handling_connections;
-        ulong records_handled;
-        ulong total_bytes;
-        ulong total_records;
+
+        /***********************************************************************
+
+            The total number of bytes sent through the client connections.
+
+        ***********************************************************************/
+
+        ulong bytes_sent;
+
+        /***********************************************************************
+
+            Creates a new instance of this struct, populated with the
+            corresponding stats counter values from node.
+
+            Params:
+                node = node to get statistics from
+
+            Returns:
+                an instance of this struct populated with the correspondent
+                values in node.
+
+        ***********************************************************************/
+
+        static typeof(*this) set ( IDmqNodeInfo node )
+        {
+            return typeof(*this)(node.num_open_connections,
+                                 node.bytes_received,
+                                 node.bytes_sent);
+        }
     }
 
-    protected LogStats log_stats;
-
-
     /***************************************************************************
 
-        Log file
-
-    ***************************************************************************/
-
-    protected StatsLog log;
-
-
-    /***************************************************************************
-
-        Console & log file update period (milliseconds)
-
-    ***************************************************************************/
-
-    private const console_update_time = 1_000;
-
-    private const uint log_update_time = StatsLog.default_period * 1_000;
-
-
-    /***************************************************************************
-
-        Number of milliseconds elapsed since the log file was last updated
-
-    ***************************************************************************/
-
-    protected uint elapsed_since_last_log_update;
-
-
-    /***************************************************************************
-
-        Average records per second counter
-
-    ***************************************************************************/
-
-    protected SlidingAverageTime!(ulong) records_per_sec;
-
-
-    /***************************************************************************
-
-        String buffers for console output formatting.
-
-    ***************************************************************************/
-
-    private char[] records_buf, bytes_buf, memory_buf;
-
-    /***************************************************************************
-
-        Per-channel stats written to log file.
+        Definition of the per-channel statistics to log.
 
     ***************************************************************************/
 
     struct ChannelStats
     {
-        struct Item
-        {
-            /*******************************************************************
-
-                Numeric statistical value.
-
-            *******************************************************************/
-
-            ulong n;
-
-            /*******************************************************************
-
-                The title for the value to use for logging.
-
-            *******************************************************************/
-
-            char[] title;
-        }
-
         /***********************************************************************
 
-            The statistics to log.
+            The number of bytes stored in the memory queue.
 
         ***********************************************************************/
 
-        Item  bytes, records, percent;
+        ulong bytes_memory;
 
         /***********************************************************************
 
-            Resets all statistical values to 0.
+            The number of bytes stored in the overflow.
 
         ***********************************************************************/
 
-        void reset ( )
-        {
-            foreach (i, ref item; this.tupleof)
-            {
-                item.n = 0;
-            }
-        }
+        ulong bytes_overflow;
 
         /***********************************************************************
 
-            Initialises a new instance of this struct. Composes the value titles
-            from the channel id and the struct field name and leaves the values
-            at the default value of 0.
+            The number of records stored in the memory queue.
+
+        ***********************************************************************/
+
+        uint  records_memory;
+
+        /***********************************************************************
+
+            The number of records stored in the overflow.
+
+        ***********************************************************************/
+
+        uint  records_overflow;
+
+        /***********************************************************************
+
+            The relative fullness of the memory queue in percent.
+
+        ***********************************************************************/
+
+        ubyte percent_memory;
+
+
+        /***********************************************************************
+
+            Creates a new instance of this struct, populated with the
+            corresponding stats counter values from node.
 
             Params:
-                id = channel id
+                node = node to get statistics from
 
             Returns:
-                A newly initialised instance of this struct.
+                an instance of this struct populated with the correspondent
+                values in node.
 
         ***********************************************************************/
 
-        static typeof(*this) opCall ( char[] id )
+        static typeof(*this) set ( RingNode.Ring channel )
         {
-            typeof(*this) stats;
-
-            foreach (i, ref item; stats.tupleof)
+            auto stats = typeof(*this)(channel.memory_info.used_space,
+                                       channel.overflow_info.num_bytes,
+                                       channel.memory_info.length,
+                                       channel.overflow_info.num_records,
+                                       100);
+            if (auto mem_capacity = channel.memory_info.total_space)
             {
-                static const suffix = "_" ~ FieldName!(i, typeof(stats));
-
-                item.title = id ~ suffix;
+                /*
+                 * channel.memory_info.total_space == 0 would be a memory queue
+                 * of zero capacity, which should be impossible in production.
+                 * Still it's a good idea to prevent a division by 0 as this may
+                 * happen in special test builds.
+                 */
+                stats.percent_memory = cast(ubyte)((stats.bytes_memory * 100.) / mem_capacity);
             }
 
             return stats;
@@ -203,12 +178,19 @@ public class PeriodicStats : IPeriodic
 
     /***************************************************************************
 
-        Per-channel stats written to log file by channel name.
+        Stats config object, passed into constructor.
 
     ***************************************************************************/
 
-    protected ChannelStats[char[]] channel_stats;
+    protected const StatsConfig stats_config;
 
+    /***************************************************************************
+
+        Log file
+
+    ***************************************************************************/
+
+    protected StatsLog log;
 
     /***************************************************************************
 
@@ -220,226 +202,42 @@ public class PeriodicStats : IPeriodic
                 registration of periodics is usually dealt with by the Periodics
                 class, but an individual periodic can also reregister itself
                 with epoll in the situation where an error occurs)
-            id = identifying string of this periodic, used for logging
 
     ***************************************************************************/
 
     public this ( StatsConfig stats_config, EpollSelectDispatcher epoll )
     {
-        super(epoll, this.console_update_time, typeof(this).stringof);
+        super(epoll, this.log.default_period * 1000, typeof(this).stringof);
 
         this.stats_config = stats_config;
-
-        this.records_per_sec = new SlidingAverageTime!(ulong)(5, 1_000, 1_000);
 
         this.log = new StatsLog(this.stats_config.file_count,
             this.stats_config.max_file_size, this.stats_config.logfile);
     }
 
-
     /***************************************************************************
 
         Called once every second by the base class. Updates the console display,
-        and write a line to the stats log if > the specified write period (as
-        specified in the construcotr) has passed.
+        and write a line to the stats log if the write period (30s as specified
+        by StatsLog.default_period) has passed.
 
     ***************************************************************************/
 
     override protected void run ( )
     {
-        this.updateChannelStats();
+        this.log.add(NodeStats.set(this.node_info));
 
-        if (this.stats_config.console_stats_enabled)
+        foreach (action_name, action_stats; this.node_info.record_action_counters)
         {
-            this.consoleOutput();
+            this.log.addObject!("action")(action_name, action_stats);
         }
 
-        this.logOutput();
-
-        this.node_info.resetCounters();
-
-        foreach (ref stats; this.channel_stats)
+        foreach (channel; this.node_info)
         {
-            stats.reset();
-        }
-    }
-
-    /***************************************************************************
-
-        Updates the console output line.
-
-    ***************************************************************************/
-
-    protected void consoleOutput ( )
-    {
-        this.updateChannelStats();
-
-        if ( this.stats_config.console_stats_enabled )
-        {
-            DigitGrouping.format(this.node_info.num_records, this.records_buf);
-            BitGrouping.format(this.node_info.num_bytes, this.bytes_buf, "b");
-
-            this.memory_buf.length = 0;
-
-            const float Mb = 1024 * 1024;
-            size_t used, free;
-            GC.usage(used, free);
-
-            if ( used + free > 0 )
-            {
-                auto mem_allocated = cast(float)(used + free) / Mb;
-                auto mem_free = cast(float)free / Mb;
-
-                Format.format(this.memory_buf, " (Used {}Mb/Free {}Mb)",
-                    mem_allocated, mem_free);
-            }
-            else
-            {
-                Format.format(this.memory_buf, " (mem usage n/a)");
-            }
-
+            this.log.addObject!("channel")(channel.id, ChannelStats.set(channel));
         }
 
-        StaticTrace.format("  {} queue {}: {} conns, {} rec/s, {} recs ({})",
-            this.node_info.storage_type, this.memory_buf,
-            this.node_info.num_open_connections,
-            this.recordsPerSecond(), this.records_buf, this.bytes_buf).flush;
-    }
-
-    /***************************************************************************
-
-        Updates the counter of average records processed per second.
-
-        Returns:
-            updated average records per second
-
-    ***************************************************************************/
-
-    private real recordsPerSecond ( )
-    {
-        // Update the average of records processed per second
-        real rec_per_sec;
-
-        this.records_per_sec = this.node_info.records_handled;
-
-        for ( int i; i < this.console_update_time / 1_000; i++ )
-        {
-            rec_per_sec = this.records_per_sec.push;
-        }
-
-        return rec_per_sec;
-    }
-
-
-    /***************************************************************************
-
-        Gathers stats to write to the log file, and writes a line to the stats
-        log is the output period (passed to the constructor) has expired.
-
-    ***************************************************************************/
-
-    protected void logOutput ( )
-    {
-        // Update counters with bytes sent & received and records handled
-        // since last call to this method
-        this.log_stats.bytes_sent += this.node_info.bytes_sent;
-        this.log_stats.bytes_received += this.node_info.bytes_received;
-        this.log_stats.records_handled += this.node_info.records_handled;
-
-        this.elapsed_since_last_log_update += this.console_update_time;
-
-        // Output logline and reset counters when period has expired
-        if ( this.elapsed_since_last_log_update >= this.log_update_time )
-        {
-            this.log_stats.total_bytes = this.node_info.num_bytes;
-            this.log_stats.total_records = this.node_info.num_records;
-            this.log_stats.handling_connections = this.node_info.num_open_connections;
-
-            this.log.add(this.log_stats);
-
-            foreach (stats; this.channel_stats)
-            {
-                foreach (item; stats.tupleof)
-                {
-                    this.log.add(item.title, item.n);
-                }
-            }
-
-            this.log.flush();
-
-            this.elapsed_since_last_log_update -= this.log_update_time;
-            this.log_stats = LogStats.init;
-        }
-    }
-
-
-    /***************************************************************************
-
-        Updates the channel stats associative arrays. Removes any channels from
-        the stats log which no longer exist.
-
-    ***************************************************************************/
-
-    private void updateChannelStats ( )
-    {
-        // Update existing channel stats
-        foreach ( channel_info; this.node_info )
-        {
-            auto channel = cast(RingNode.Ring)channel_info;
-            assert(channel, "channel class " ~ channel.classinfo.name ~ " is not " ~ RingNode.Ring.stringof);
-            //If channel id is not duplicated when added to channel_*_title and
-            //later disappears from node_info(because the channel is removed),
-            //channel_*_title[id] would return garbage.
-
-            if (ChannelStats* stats = channel.id in this.channel_stats)
-            {
-                if (stats.bytes.n < channel.num_bytes)
-                {
-                    stats.bytes.n = channel.num_bytes;
-                }
-
-                if (stats.records.n < channel.num_records)
-                {
-                    stats.records.n = channel.num_records;
-                }
-
-                // If a channel has a zero capacity, don't divide by it but
-                // report 100% usage. This is currently not allowed as a
-                // configuration value but making it robust doesn't hurt.
-                if (ulong capacity_bytes = channel.capacity_bytes)
-                {
-                    stats.percent.n = cast(ulong)(stats.bytes.n * 100.f / capacity_bytes);
-                }
-                else
-                {
-                    stats.percent.n = 100;
-                }
-            }
-            else
-            {
-                this.channel_stats[channel.id] = ChannelStats(channel.id);
-            }
-        }
-
-        // Check for dead channels in stats. One shouldn't remove elements while
-        // iterating over an associative array so we have to make a list of
-        // removed channels first, then iterate over that list and remove the
-        // channels from this.channel_stats.
-        char[][] to_remove; // Note: this will allocate in the case where a
-            // channel has been removed. This is very rare.
-        foreach ( id, stats; this.channel_stats )
-        {
-            if ( !(id in node_info) )
-            {
-                to_remove ~= id;
-            }
-        }
-
-        // Remove dead channels from stats
-        foreach ( id; to_remove )
-        {
-            this.channel_stats.remove(id);
-        }
+        this.log.flush();
     }
 }
 
