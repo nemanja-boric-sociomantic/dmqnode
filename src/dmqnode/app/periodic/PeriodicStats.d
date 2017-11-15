9799243a2743cd19e9dc82c6835bc0690688f3ca
davideckardt <david.eckardt@sociomantic.com>
2017-11-15 17:51:05 +0100

    Move `ChannelStats` into a separate module
    `PeriodicStats` is going to be removed while `ChannelStats` will keep being
    used.

--- src/dmqnode/app/periodic/PeriodicStats.d
+++ src/dmqnode/app/periodic/PeriodicStats.d
@@ -12,7 +12,6 @@ module dmqnode.app.periodic.PeriodicStats;
 
 
 import dmqnode.app.config.StatsConfig;
-import dmqnode.storage.Ring;
 import dmqnode.node.IDmqNodeInfo;
 
 import swarm.util.node.log.Stats;
@@ -30,91 +29,7 @@ import ocean.util.log.Log;
 
 public class PeriodicStats
 {
-    /***************************************************************************
-
-        Definition of the per-channel statistics to log in addition to those
-        automatically written by ChannelsNodeStats.
-
-    ***************************************************************************/
-
-    struct ChannelStats
-    {
-        /***********************************************************************
-
-            The number of bytes stored in the memory queue.
-
-        ***********************************************************************/
-
-        ulong bytes_memory;
-
-        /***********************************************************************
-
-            The number of bytes stored in the overflow.
-
-        ***********************************************************************/
-
-        ulong bytes_overflow;
-
-        /***********************************************************************
-
-            The number of records stored in the memory queue.
-
-        ***********************************************************************/
-
-        uint  records_memory;
-
-        /***********************************************************************
-
-            The number of records stored in the overflow.
-
-        ***********************************************************************/
-
-        uint  records_overflow;
-
-        /***********************************************************************
-
-            The relative fullness of the memory queue in percent.
-
-        ***********************************************************************/
-
-        ubyte percent_memory;
-
-
-        /***********************************************************************
-
-            Creates a new instance of this struct, populated with the
-            corresponding stats counter values from node.
-
-            Params:
-                node = node to get statistics from
-
-            Returns:
-                an instance of this struct populated with the correspondent
-                values in node.
-
-        ***********************************************************************/
-
-        static typeof(*this) set ( RingNode.Ring channel )
-        {
-            auto stats = typeof(*this)(channel.memory_info.used_space,
-                                       channel.overflow_info.num_bytes,
-                                       cast(uint)channel.memory_info.length,
-                                       channel.overflow_info.num_records,
-                                       100);
-            if (auto mem_capacity = channel.memory_info.total_space)
-            {
-                /*
-                 * channel.memory_info.total_space == 0 would be a memory queue
-                 * of zero capacity, which should be impossible in production.
-                 * Still it's a good idea to prevent a division by 0 as this may
-                 * happen in special test builds.
-                 */
-                stats.percent_memory = cast(ubyte)((stats.bytes_memory * 100.) / mem_capacity);
-            }
-
-            return stats;
-        }
-    }
+    import dmqnode.app.stats.ChannelStats;
 
     /***************************************************************************
 
